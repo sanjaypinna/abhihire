ffa562feba3c98337608644ed80e5b8ae1a11121
sanjaypinna <sanjaypinna265@gmail.com>
2025-06-29 17:32:35 +0530

    feat: add monthly and overall stats API routes

---
+++ app/api/monthlyStats/route.ts
@@ -0,0 +1,34 @@
+// app/api/jobs/route.ts
+import { NextResponse } from "next/server";
+
+
+
+export async function POST(request: Request) {
+  try {
+    const jsonData = await request.json();
+
+    const { volunteer_id, month } = jsonData;
+
+    // Create a FormData object instead of JSON
+    const formData = new FormData();
+
+    formData.append("volunteer_id", volunteer_id);
+    formData.append("month", month);  
+
+    const apiEndpoint = "https://powerapps.sbs/AbhiHire/sapanel/GetVolunteer_MonthStats.php";
+
+    const response = await fetch(apiEndpoint, {
+      method: "POST",
+      body: formData, // Send as FormData
+    });
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    return NextResponse.json(result);
+  } catch (error) {
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
